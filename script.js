8c658b39e0a54ec257b957f1be168dc5d931ce47
Giovanni Ghinet <ghinetgiovanni88@gmai.com>
2026-04-02 15:33:27 +0200

    collegato js all'hatml e stampato il log quando clicchiamo il bottone

--- script.js
+++ script.js
@@ -1,1 +1,13 @@
+const bottonePerAccendere = document.querySelector('#bottone');
+const StatoLampadina = document.querySelector('#lampadina');
+
+
+function clickPerAccendere() {
+    console.log('Bottone per accendere la lampadina');
+    
+}
+
+bottonePerAccendere.addEventListener(
+    'click', clickPerAccendere
+)
 
